039103ae2390ecc3c1e48463bf797a00ff60fcc8
Juan Gomez <juangom73@gmaill.com>
2023-10-28 14:53:07 -0400

    Add comments

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -3,6 +3,8 @@ import { Pipe, PipeTransform } from "@angular/core";
 // fernando | toggleCase  = 'FERNANDO'
 // FERNANDO | toggleCase  = 'fernando'
 
+// To tell Angular the existence of this Pipe, we need to add it to
+// the "declaration" of the "products.module.ts"
 @Pipe({
   name: 'toggleCase'
 })
